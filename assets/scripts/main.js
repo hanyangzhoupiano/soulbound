f925797ac98c38a4f8518ad355345ba9e1834da3
hanyangzhoupiano <130773491+hanyangzhoupiano@users.noreply.github.com>
2025-06-21 11:47:20 -0400

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,16 @@
+/* CONSTANTS */
 
+const canvas = $("#canvas");
+const ctx = canvas.getContext("2d");
+
+/* FUNCTIONS */
+
+let clearCanvas = () => ctx.clearRect(0, 0, canvas.width, canvas.height);
+let drawRect = (x, y, width, height, color) => {ctx.fillStyle = color; ctx.fillRect(x, y, width, height)};
+
+function draw() {
+    clearCanvas()
+    drawRect(Math.random() * 350 + 50, Math.random() * 350 + 50, Math.random() * 350 + 50, Math.random() * 350 + 50, "#000000")
+}
+
+setInterval(draw, 20)

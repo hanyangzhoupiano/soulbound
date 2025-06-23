2e068e09e453c7e8dbdb11e0ddd41da2737f4818
hanyangzhoupiano <130773491+hanyangzhoupiano@users.noreply.github.com>
2025-06-23 17:38:11 -0400

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,15 +2,16 @@
 
 const canvas = $("#canvas");
 const ctx = canvas.getContext("2d");
+const FPS = 50
 
 /* FUNCTIONS */
 
 let clearCanvas = () => ctx.clearRect(0, 0, canvas.width, canvas.height);
 let drawRect = (x, y, width, height, color) => {ctx.fillStyle = color; ctx.fillRect(x, y, width, height)};
+let drawText = (x, y, font, size, text) => {ctx.font = `${size}px ${font}`; ctx.fillText(text, x, y)};
 
 function draw() {
     clearCanvas()
-    drawRect(Math.floor(Math.random() * 350 + 50), Math.floor(Math.random() * 350 + 50), 50, 50, "#000000")
 }
 
-setInterval(draw, 50)
+setInterval(draw, 1000 / FPS)
